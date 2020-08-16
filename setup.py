86d3c2419f8a45678c4e1d2a8d96ff583abf3cd0
amanchourasiya <amanchourasia6@gmail.com>
2020-08-16 06:42:58 +0000

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-rom distutils.core import setup
+from distutils.core import setup
 setup(
   name = 'obify',         # How you named your package folder (MyLib)
   packages = ['obify'],   # Chose the same as "name"
