ddf4846497ad44e0d310bbef96c9f36d96792abf
amanchourasiya <amanchourasia6@gmail.com>
2020-08-16 07:01:02 +0000

    Updated readme with sample code

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='GNU GPL-v3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library provides a set of data structures that will work on python objects similar to collections framework in java',   # Give a short description about your library
+  long_description = long_description,
+  long_description_content_type="text/markdown",
   author = 'Aman Chourasiya',                   # Type in your name
   author_email = 'aman@amanchourasiya.com',      # Type in your E-Mail
   url = 'https://github.com/amanchourasiya/obify',   # Provide either the link to your github or to your website
@@ -18,7 +20,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU GPL-v3',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License (GPL)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
   ],
 )
